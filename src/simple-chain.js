07dcb9c220889ac931b1776578c9ba3cc43a0a30
Slava Slivka <aphelion.unet@gmail.com>
2022-05-02 13:29:22 +0300

    feat: add simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  data: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.data.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    value === null ? this.data.push(`null`) : this.data.push(value);
+
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (this.data[position - 1] === undefined) {
+      this.data = [];
+      throw new Error(`You can't remove incorrect link!`);
+    }
+    this.data.splice(position - 1, 1);
+
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.data.reverse();
+
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const data = `( ${this.data.join(' )~~( ')} )`;
+    
+    this.data = [];
+    return data;
   }
 };
 
