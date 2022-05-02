5980341f7a4cfb514395babf1377f76d91370f44
Slava Slivka <aphelion.unet@gmail.com>
2022-05-02 13:34:08 +0300

    feat: add encode-line task

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  const arr = [...new Set(str.split(``))];
+
+  arr.forEach(it => {
+    const regexp = new RegExp(`[${it}]{1,}`, `g`);
+    str = str.replace(regexp, match => {
+      return match.length > 1 ? `${match.length}${match[0]}` : match[0];
+    });
+  });
+
+  return str;
 }
 
 module.exports = {
