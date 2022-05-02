e35e4eb61b38d92bb50ac6b859b2959ed920a358
Slava Slivka <aphelion.unet@gmail.com>
2022-05-02 13:32:11 +0300

    feat: add vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,57 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
+    this._alphabet = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
+    this._TABULA_RECTA = [];
+    
+    for (let i = 0; i < this._alphabet.length; i++) {
+      this._TABULA_RECTA[i] = this._alphabet.slice(i).concat(this._alphabet.slice(0, i));
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  encrypt(message, key) {
+    if (!message || !key) throw new Error(`Incorrect arguments!`);
+    
+    message = message.toUpperCase();
+    key = key.repeat(message.length / key.length + 1).toUpperCase();
+    let str = ``;
+    
+    for (let i = 0; i < message.length; i++) {
+      let row = this._alphabet.indexOf(message[i]);
+      let col = this._alphabet.indexOf(key[i]);
+      if (row !== -1 && col !== -1) {
+        str += this._TABULA_RECTA[row][col];
+      } else {
+        str+= message[i];
+        message = message.replace(message[i], ``);
+        i--;
+      }
+    }
+    
+    return this.direct ? str : str.split(``).reverse().join(``);
+  }
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) throw new Error(`Incorrect arguments!`);
+    
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.repeat(encryptedMessage.length / key.length + 1).toUpperCase();
+    
+    let str = ``;
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      const row = this._alphabet.indexOf(key[i]);
+      const col = this._TABULA_RECTA[row].indexOf(encryptedMessage[i]);
+
+      if (col !== -1) {
+        str += this._alphabet[col];
+      } else {
+        str += encryptedMessage[i];
+        key = key.substring(0, i) + encryptedMessage[i] + key.substring(i, encryptedMessage.length);
+      }
+    }
+ 
+    return this.direct ? str : str.split(``).reverse().join(``);
   }
 }
 
