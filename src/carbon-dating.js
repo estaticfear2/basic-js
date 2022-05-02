a47058264da96cc59f581b4d2e0c2b2fcf21f2c6
Slava Slivka <aphelion.unet@gmail.com>
2022-05-02 13:22:16 +0300

    feat: add carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,6 +2,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
+const LN_2 = 0.693;
 
 /**
  * Determine the age of archeological find by using
@@ -17,9 +18,9 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== `string` || sampleActivity <= 0 || sampleActivity >= MODERN_ACTIVITY) return false;
+  return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (LN_2 / HALF_LIFE_PERIOD)) || false;
 }
 
 module.exports = {
