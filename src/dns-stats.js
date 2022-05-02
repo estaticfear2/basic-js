3233cf1f6fadca98abad3cfaa140c1ed5ee1f29d
Slava Slivka <aphelion.unet@gmail.com>
2022-05-02 13:33:47 +0300

    feat: add dns-stats task

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let result = {};
+  
+  domains.forEach(domain => {
+    let domainKey = ``
+    const dns = domain.split(`.`).reverse();
+    dns.forEach(it => {
+      domainKey += `.${it}`;
+      if (result.hasOwnProperty(domainKey)) {
+        result[domainKey] += 1;
+      } else {
+        result[domainKey] = 1;
+      }
+    });
+  });
+  
+  return result;
 }
 
 module.exports = {
