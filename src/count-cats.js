7ba47d4718c9c15c712ca22c6713a5e25cc2e2c1
Slava Slivka <aphelion.unet@gmail.com>
2022-05-02 13:18:03 +0300

    feat: add count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(arr) {
+  return arr.flat().reduce((acc, it) => {
+    return it === '^^' ? ++acc : acc;
+  }, 0);
 }
 
 module.exports = {
